0b366473fb3b63657edcd0893c9862aec9b98784
Gabor Vajda <vajdagabor@users.noreply.github.com>
2025-01-12 10:25:26 +0100

    Add min and max props to Button

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -2,16 +2,21 @@ import { Button } from "./Button"
 
 interface Props {
   n?: number
+  min?: number
+  max?: number
   onDecrement?: React.MouseEventHandler
   onIncrement?: React.MouseEventHandler
 }
 
-export function Counter({ n = 0, onDecrement, onIncrement }: Props) {
+export function Counter({ n = 0, min, max, onDecrement, onIncrement }: Props) {
+  const canDecrease = min === undefined ? true : n > min
+  const canIncrease = max === undefined ? true : n < max
+
   return (
     <div className="Counter">
-      <Button label="−" onClick={onDecrement} />
+      <Button label="−" onClick={onDecrement} disabled={!canDecrease} />
       <span>{n}</span>
-      <Button label="+" onClick={onIncrement} />
+      <Button label="+" onClick={onIncrement} disabled={!canIncrease} />
     </div>
   )
 }
