13d876e7bb9adceebd9738e25ca6010bb29f463c
Gabor Vajda <vajdagabor@users.noreply.github.com>
2025-01-18 15:13:29 +0100

    Fix the reducer test

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -5,7 +5,16 @@ import { Field } from './model'
 const baseState: State = {
   size: 3,
   fields: [],
-  fieldRotations: new Map(),
+  fieldRotations: new Map([
+    [1, 0],
+    [2, 0],
+    [3, 0],
+    [4, 0],
+    [5, 0],
+    [6, 0],
+    [7, 0],
+    [8, 0],
+  ]),
   playerDirection: 'down',
 }
 
@@ -13,7 +22,7 @@ describe('KEYDOWN', () => {
   it('should return the same state when null is at the bottom', () => {
     const fields: Field[] = [1, 2, 3, 4, 5, 6, 7, 8, null]
     const state = { ...baseState, fields }
-    const result = reducer(state, { type: 'KEYDOWN' })
+    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
     const expected = { ...state }
 
     expect(result).toEqual(expected)
@@ -22,17 +31,17 @@ describe('KEYDOWN', () => {
   it('should change the player orientation', () => {
     const fields: Field[] = [1, 2, 3, 4, 5, 6, 7, 8, null]
     const state: State = { ...baseState, fields, playerDirection: 'up' }
-    const result = reducer(state, { type: 'KEYDOWN' })
+    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
     const expected = { ...state, playerDirection: 'down' }
 
     expect(result).toEqual(expected)
   })
 
   it('should move the player down', () => {
-    const fields: Field[] = [1, 2, 3, 4, 5, null, 7, 8, 9]
+    const fields: Field[] = [1, 2, 3, 4, 5, null, 6, 7, 8]
     const state: State = { ...baseState, fields }
-    const result = reducer(state, { type: 'KEYDOWN' })
-    const expected = { ...state, fields: [1, 2, 3, 4, 5, 9, 7, 8, null] }
+    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
+    const expected = { ...state, fields: [1, 2, 3, 4, 5, 8, 6, 7, null] }
 
     expect(result).toEqual(expected)
   })
