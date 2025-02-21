031594cd8547432210ed86820852c6e41144ecab
Gabor Vajda <vajdagabor@users.noreply.github.com>
2025-02-21 11:20:14 +0100

    Adjust min and max size

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
-export const minSize = 3
-export const maxSize = 9
-export const initialSize = 3
-export const maxRotation = 10
+export const minSize = 2
+export const maxSize = 7
+export const initialSize = 4
+export const maxRotation = 10 // degrees
 export const shuffleDelay = 1 // ms
