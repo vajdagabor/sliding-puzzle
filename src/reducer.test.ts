4b8e47feafc7d49cd507894c2b5083ce647c064a
Gabor Vajda <vajdagabor@users.noreply.github.com>
2025-03-06 12:15:15 +0100

    Fix reducer tests

--- src/reducer.test.ts
+++ src/reducer.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest'
 import { State, reducer } from './reducer'
-import { Field } from './model'
+import { Field } from './types'
 
 const baseState: State = {
   size: 3,
@@ -16,13 +16,19 @@ const baseState: State = {
     [8, 0],
   ]),
   playerDirection: 'down',
+  isShuffling: false,
+  shuffleSteps: 0,
 }
 
 describe('KEYDOWN', () => {
   it('should return the same state when null is at the bottom', () => {
     const fields: Field[] = [1, 2, 3, 4, 5, 6, 7, 8, null]
     const state = { ...baseState, fields }
-    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
+    const result = reducer(state, {
+      type: 'KEY_ARROW_PRESSED',
+      direction: 'down',
+      rotation: 0,
+    })
     const expected = { ...state }
 
     expect(result).toEqual(expected)
@@ -31,7 +37,11 @@ describe('KEYDOWN', () => {
   it('should change the player orientation', () => {
     const fields: Field[] = [1, 2, 3, 4, 5, 6, 7, 8, null]
     const state: State = { ...baseState, fields, playerDirection: 'up' }
-    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
+    const result = reducer(state, {
+      type: 'KEY_ARROW_PRESSED',
+      direction: 'down',
+      rotation: 0,
+    })
     const expected = { ...state, playerDirection: 'down' }
 
     expect(result).toEqual(expected)
@@ -40,7 +50,11 @@ describe('KEYDOWN', () => {
   it('should move the player down', () => {
     const fields: Field[] = [1, 2, 3, 4, 5, null, 6, 7, 8]
     const state: State = { ...baseState, fields }
-    const result = reducer(state, { type: 'KEYDOWN', rotation: 0 })
+    const result = reducer(state, {
+      type: 'KEY_ARROW_PRESSED',
+      direction: 'down',
+      rotation: 0,
+    })
     const expected = { ...state, fields: [1, 2, 3, 4, 5, 8, 6, 7, null] }
 
     expect(result).toEqual(expected)
