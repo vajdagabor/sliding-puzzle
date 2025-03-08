4a4ecbfe392b6140059e5828c78c6da03eb2a7a2
Gabor Vajda <vajdagabor@users.noreply.github.com>
2025-03-08 09:06:27 +0100

    Make rotations and transitions more subtle

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 export const minSize = 2
 export const maxSize = 7
 export const initialSize = 4
-export const maxRotation = 10 // degrees
+export const maxRotation = 5 // degrees
 export const shuffleDelay = 1 // ms
 
 export const sizes = Array.from(
